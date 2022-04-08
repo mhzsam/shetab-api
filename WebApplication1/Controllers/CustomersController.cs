dc4926f371f860ff3b5010846efe112dcc895c2d
mohamad zarabi <zarab@gmail.com>
2022-04-08 23:16:57 +0430

    df

---
+++ WebApplication1/Controllers/CustomersController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private EshopApi_DBContext _context;
+
+        public CustomersController(EshopApi_DBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetCustomer()
+        {
+            return new ObjectResult(_context.Customers);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomer( int id )
+        {
+            var customer =await _context.Customers.SingleOrDefaultAsync(c => c.CustomerId == id);
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostCustomer([FromBody] Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetCustomer", new { id = customer.CustomerId }, customer);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult>  Putcustomer([FromRoute] int id, [FromBody] Customer customer)
+        {
+            _context.Entry(customer).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return Ok(customer);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult>  Deletecustomer([FromRoute] int id)
+        {
+
+            var customer =await _context.Customers.FindAsync(id);
+            _context.Customers.Remove(customer);
+            _context.SaveChangesAsync();
+            return Content("it whas delete");
+        }
+
+    }
+}
---
+++ WebApplication1/Migrations/20220408162721_start.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    public partial class start : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstNamr = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    LastName = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    Email = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    Phone = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    Address = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    City = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    State = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    ZipCode = table.Column<string>(type: "nchar(10)", fixedLength: true, maxLength: 10, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    Size = table.Column<int>(type: "int", nullable: true),
+                    Varienty = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: true),
+                    Status = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SalesPerson",
+                columns: table => new
+                {
+                    SalesPersonId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    LastName = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    Email = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    Phone = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    Address = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    City = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    State = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    ZipCode = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalesPerson", x => x.SalesPersonId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Total = table.Column<double>(type: "float", nullable: true),
+                    Status = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: true),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    SalesPersonId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customer",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "CustomerId");
+                    table.ForeignKey(
+                        name: "FK_Orders_SalesPerson",
+                        column: x => x.SalesPersonId,
+                        principalTable: "SalesPerson",
+                        principalColumn: "SalesPersonId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderItems",
+                columns: table => new
+                {
+                    OrderItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    QuanTity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItems", x => x.OrderItemId);
+                    table.ForeignKey(
+                        name: "FK_OrderItems_Orders",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "OrderId");
+                    table.ForeignKey(
+                        name: "FK_OrderItems_Products",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_OrderId",
+                table: "OrderItems",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_ProductId",
+                table: "OrderItems",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_SalesPersonId",
+                table: "Orders",
+                column: "SalesPersonId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderItems");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "SalesPerson");
+        }
+    }
+}
